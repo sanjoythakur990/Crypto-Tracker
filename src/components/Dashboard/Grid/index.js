980244bccad2c0e64233a31328994c016c5548f9
Sanjoy <sanjoythakur990@gmail.com>
2025-01-21 01:23:12 +0530

    fixed some bugs and updated project versions

--- src/components/Dashboard/Grid/index.js
+++ src/components/Dashboard/Grid/index.js
@@ -5,7 +5,11 @@ import TrendingDownRoundedIcon from "@mui/icons-material/TrendingDownRounded";
 
 function Grid({ coin, i }) {
   return (
-    <div className={`grid-container ${coin.price_change_percentage_24h < 0 && "grid-container-red"}`}>
+    <div
+      className={`grid-container ${
+        coin.price_change_percentage_24h < 0 && "grid-container-red"
+      }`}
+    >
       <div className="info-flex">
         <img src={coin.image} className="coin-logo" />
         <div className="name-col">
@@ -44,8 +48,12 @@ function Grid({ coin, i }) {
         >
           ${coin.current_price.toLocaleString()}
         </h3>
-        <p className="total-volume">Total Volume : ${coin.total_volume.toLocaleString()}</p>
-        <p className="market-cap">Market Cap : ${coin.market_cap.toLocaleString()}</p>
+        <p className="total-volume">
+          Total Volume : ${coin.total_volume.toLocaleString()}
+        </p>
+        <p className="market-cap">
+          Market Cap : ${coin.market_cap.toLocaleString()}
+        </p>
       </div>
     </div>
   );
